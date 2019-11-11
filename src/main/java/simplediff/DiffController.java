e8773a625fab09f1bba28d24e82d503be1bb0399
Alex Nguyen <alexnguyen@live.ca>
2019-11-10 20:09:23 -0500

    Initial commit

---
+++ src/main/java/simplediff/DiffController.java
@@ -0,0 +1,94 @@
+package simplediff;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class DiffController {
+
+    @GetMapping("/diff")
+    public String getDiff() {
+        prepareCommands();
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("bash", "-c", "./gumtree/bin/gumtree webdiff targetBranch sourceBranch");
+        try {
+            Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+
+            if (exitVal == 0) {
+                finishedCommands();
+                return output.toString();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Couldn't perform operation";
+    }
+
+    private void prepareCommands() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("bash", "-c", "git clone -b master https://github.com/itsAlexNguyen/samples.git targetBranch && git clone -b testdiff https://github.com/itsAlexNguyen/samples.git sourceBranch");
+
+        try {
+            Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                return;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void finishedCommands() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("bash", "-c", "rm -rf targetBranch && rm -rf sourceBranch");
+
+        try {
+            Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                return;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/simplediff/models/DiffRequest.java
@@ -0,0 +1,26 @@
+package simplediff.models;
+
+public class DiffRequest {
+    private final String gitRepo;
+    private final String srcBranch;
+    private final String targetBranch;
+
+
+    public DiffRequest(String gitRepo, String srcBranch, String targetBranch) {
+        this.gitRepo = gitRepo;
+        this.srcBranch = srcBranch;
+        this.targetBranch = targetBranch;
+    }
+
+    public String getGitRepo() {
+        return gitRepo;
+    }
+
+    public String getTargetBranch() {
+        return targetBranch;
+    }
+
+    public String getSrcBranch() {
+        return srcBranch;
+    }
+}
