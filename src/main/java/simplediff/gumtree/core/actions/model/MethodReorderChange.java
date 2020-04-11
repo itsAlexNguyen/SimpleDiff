d2d6fb23cf2e1b36cd02b992e73313d4bb66e9b9
Phillip Pavlich <pavlicpm@mcmaster.ca>
2020-04-11 17:02:55 -0400

    method reordering, simplify as a single update when a change has been made

--- src/main/java/simplediff/gumtree/core/actions/model/MethodReorderChange.java
+++ src/main/java/simplediff/gumtree/core/actions/model/MethodReorderChange.java
@@ -22,7 +22,7 @@ public class MethodReorderChange extends Change {
 
   public static MethodReorderChange createMethodReorderChange(final List<String> initialOrder, final List<String> finalOrder, boolean update){
     if(update) {
-      changeCounter.put("Additions", changeCounter.get("Additions") + 1);
+      changeCounter.put("Updates", changeCounter.get("Updates") + 1);
     }
     final String insertPlaceholder =
         "&lt;div class=\"row\"&gt;&lt;div class=\"col-6\"&gt; Initial order &lt;ul&gt;&lt;li&gt;%s&lt;/li&gt;&lt;/ul&gt;&lt;/div&gt;&lt;div class=\"col-6\"&gt;" +
